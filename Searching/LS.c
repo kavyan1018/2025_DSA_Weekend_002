9fa8bf2dc09babc18bace31dc20d4e45e5dfde19
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-08 18:27:07 +0530

    first commit

---
+++ Searching/LS.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int arr[5] = {12, 40, 60, 30, 40};
+    int search, i, found = 0;
+
+    printf("Enter the number to search: ");
+    scanf("%d", &search);
+
+    // Linear Search
+
+    for (i = 0; i < 5; i++)
+    {
+        if(arr[i] == search){
+            found = 1;
+            break;
+        }
+    }
+
+
+    if(found){
+        printf("Number %d found at index %d\n", search, i+1);
+    }
+    else{
+        printf("Not found\n");
+    }
+
+}
